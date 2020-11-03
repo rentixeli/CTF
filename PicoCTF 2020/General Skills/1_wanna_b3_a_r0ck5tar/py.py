6c09c2397215390334cd179dd0bf210237cd6f45
rentixeli <73320873+rentixeli@users.noreply.github.com>
2020-11-03 14:26:34 -0500

    Add files via upload

---
+++ PicoCTF 2020/General Skills/1_wanna_b3_a_r0ck5tar/py.py
@@ -0,0 +1,19 @@
+Rocknroll = True
+Silence = False
+a_guitar = 10
+Tommy = 44
+Music = 170
+if 1==1:
+        Tommy = 66
+        print(Tommy)
+        Music = 79
+        Jamming = 78
+        print(Music)
+        print(Jamming)
+        Tommy = 74
+        print(Tommy)
+        print (Music)
+        Rock = 86
+        print(Rock)
+        Tommy = 73
+        print(Tommy)
